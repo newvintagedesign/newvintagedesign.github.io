4c7abb2902a908f3cd2aada4250b58acc1797064
Charlie Hay <charliehay@Charlies-MacBook.local>
2019-04-23 21:49:30 -0400

    referendum & window update 2019

--- app/js/reroute.js
+++ app/js/reroute.js
@@ -1,3 +1,9 @@
-var url = location.origin + '/location/';
+// 12:00 am on April 28th
+// May 5th 11:59pm.
+var date = new Date().getTime();
 
-window.location = url;
+if (date > 1556424000000 && date < 1557115199000) {
+  window.location = location.origin + "/location/";
+} else {
+  window.location = location.origin + "/closed/";
+}
